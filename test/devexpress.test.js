7bdd2977efecca3e8cf8ee3ce4299d74939a5341
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-17 07:35:36 +0700

    interacting with html inputs

---
+++ test/devexpress.test.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer')
+
+describe('Test DevExpress Testcafe Example page', () => {
+  it('Interacting with Inputs', async () => {
+    const browser = await puppeteer.launch({ 
+      headless: false, 
+      defaultViewport: null,
+      slowMo: 10,
+      devtools: false
+    })
+    const page = await browser.newPage()
+    await page.goto('https://devexpress.github.io/testcafe/example/')
+    await page.type('#developer-name', 'Albert', { delay: 100}) // type a value into a textbox
+    await new Promise(r => setTimeout(r, 5000)) // wait for 5 seconds
+
+    await browser.close()
+  })
+})
