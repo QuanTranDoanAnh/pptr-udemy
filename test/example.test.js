667b26d49b0bf898aebb3b815d4ca1d03f58b93b
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-24 08:27:12 +0900

    18. Test Hooks

--- test/example.test.js
+++ test/example.test.js
@@ -1,14 +1,43 @@
 const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test', () => {
-  it('should launch the browser', async () => {
-    const browser = await puppeteer.launch({
+  let browser
+  let page
+
+  before(async () => {
+    browser = await puppeteer.launch({
       headless: false,
       defaultViewport: null,
-      slowMo: 250, //slowdown for user to see effects
+      slowMo: 10, //slowdown for user to see effects
       devtools: false
     })
-    const page = await browser.newPage()
+    page = await browser.newPage()
+    await page.setDefaultTimeout(10000)
+    await page.setDefaultNavigationTimeout(20000)
+  })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  beforeEach(async () => {
+    // Runs before each test step
+  })
+
+  afterEach(async () => {
+    // Runs after each test step
+  })
+
+  it('should launch the browser', async () => {
+    // const browser = await puppeteer.launch({
+    //   headless: false,
+    //   defaultViewport: null,
+    //   slowMo: 250, //slowdown for user to see effects
+    //   devtools: false
+    // })
+    // const page = await browser.newPage()
+    // await page.setDefaultTimeout(10000)
+    // await page.setDefaultNavigationTimeout(20000)
     await page.goto('http://example.com')
     await page.waitForSelector('h1')
     await page.goto('https://dev.to')
@@ -17,6 +46,6 @@ describe('My First Puppeteer Test', () => {
     await page.waitForSelector('h1')
     await page.goForward()
     await page.waitForSelector('#topbar')
-    await browser.close()
+    // await browser.close()
   })
 })
