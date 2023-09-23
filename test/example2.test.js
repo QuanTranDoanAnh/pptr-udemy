bba7beede88b9cad99a282ab173663fe27de291c
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-24 01:16:18 +0900

    13. Assertions

--- test/example2.test.js
+++ test/example2.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('New Puppeteer Test with Example.com', () => {
   it('should launch the browser for new test', async () => {
@@ -14,8 +15,11 @@ describe('New Puppeteer Test with Example.com', () => {
     const url = await page.url()
     const text = await page.$eval('h1', el => el.textContent)
     const count = await page.$$eval('p', element => element.length)
-    console.log('Text in the H1: ' + text)
-    console.log('Number of P tags on the page: ' + count)
+    
+    expect(title).to.be.a('string', 'Example Domain')
+    expect(url).to.include('example.com')
+    expect(text).to.be.a('string', 'Example Domain')
+    expect(count).to.equal(2)
     await browser.close()
   })
 })
